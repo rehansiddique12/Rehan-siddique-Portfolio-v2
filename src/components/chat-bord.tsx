38f41660065113ba042c487804fe39b0e9211543
M.REHAN <rehansiddique962@gmail.com>
2025-09-27 16:24:14 +0500

    typing

--- src/components/chat-bord.tsx
+++ src/components/chat-bord.tsx
@@ -1,21 +1,44 @@
-import { useState } from "react";
+import "ldrs/react/Bouncy.css";
+import { cn } from "@/lib/utils";
+import { Bouncy } from "ldrs/react";
+import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useEffect, useRef, useState } from "react";
 import { Separator } from "@/components/ui/separator";
 import { BotMessageSquare, CircleX, Send } from "lucide-react";
-import { cn } from "@/lib/utils";
-import { Input } from "./ui/input";
+
 const Chatbord = () => {
+  const [typing, setTyping] = useState<boolean>(false);
+  const inputref = useRef<HTMLInputElement | null>(null);
+  const typingTimer = useRef<NodeJS.Timeout | null>(null);
   const [open, setOpen] = useState<boolean | undefined>(undefined);
+
+  useEffect(() => {
+    if (open) {
+      inputref.current?.focus();
+    }
+  }, [open]);
+
+  const handleTyping = () => {
+    setTyping(true);
+
+    if (typingTimer.current) clearTimeout(typingTimer.current);
+
+    typingTimer.current = setTimeout(() => {
+      setTyping(false);
+    }, 4000);
+  };
+
   const handleToggle = (e: React.MouseEvent) => {
     e.stopPropagation();
     setOpen((prev) => !prev);
   };
   return (
     <div>
-      <div className="z-[10] right-6 md:right-8 fixed top-[45em] md:top-[47em] ">
+      <div className="z-[10] right-6 md:right-8 fixed top-[43em] md:top-[47em] ">
         <button
           onClick={handleToggle}
-          className="rounded-full items-center justify-center whitespace-nowrap  text-sm font-medium transition-transform duration-200 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 group relative animate-rainbow cursor-pointer border-0 bg-[linear-gradient(#ffffff33),linear-gradient(#fff_50%,rgba(255,255,255,0.6)_80%,rgba(0,0,0,0)),linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))] bg-[length:200%] text-foreground [background-clip:padding-box,border-box,border-box] [background-origin:border-box] [border:calc(0.08*1rem)_solid_transparent] before:absolute before:bottom-[-20%] before:left-1/2 before:z-[0] before:h-[20%] before:w-[60%] before:-translate-x-1/2 before:animate-rainbow before:bg-[linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))] before:[filter:blur(calc(0.8*1rem))] dark:bg-[linear-gradient(#121213,#121213),linear-gradient(#121213_50%,rgba(18,18,19,0.6)_80%,rgba(18,18,19,0)),linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))] hover:scale-105 active:scale-95  p-4 inline-flex"
+          className="rounded-full items-center justify-center whitespace-nowrap  text-sm font-medium transition-transform duration-200 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 group relative animate-rainbow cursor-pointer border-0 bg-[linear-gradient(#ffffff33),linear-gradient(#fff_50%,rgba(255,255,255,0.6)_80%,rgba(0,0,0,0)),linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))] bg-[length:300%] text-foreground [background-clip:padding-box,border-box,border-box] [background-origin:border-box] [border:calc(0.08*1rem)_solid_transparent] before:absolute before:bottom-[-20%] before:left-1/2 before:z-[0] before:h-[20%] before:w-[60%] before:-translate-x-1/2 before:animate-rainbow before:bg-[linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))] before:[filter:blur(calc(0.8*1rem))] dark:bg-[linear-gradient(#121213,#121213),linear-gradient(#121213_50%,rgba(18,18,19,0.6)_80%,rgba(18,18,19,0)),linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))] hover:scale-105 active:scale-95  p-4 inline-flex"
         >
           <BotMessageSquare
             className={cn("size-7  transition-all duration-500 ease-in-out", {
@@ -25,10 +48,14 @@ const Chatbord = () => {
         </button>
 
         {open && (
-          <div className="duration-700 absolute bottom-16  right-10 w-72 rounded-lg rounded-br-none shadow-xl z-50 overflow-hidden border-gray-400/15  bg-transparent backdrop-blur-sm">
+          <div className="duration-700 absolute bottom-18  right-10 w-72 rounded-lg rounded-br-none shadow-xl z-50 overflow-hidden border-gray-400/15  bg-transparent backdrop-blur-md">
             <div className="flex items-center justify-between p-4">
               <span className="font-semibold text-xl">Chat Board</span>
-              <Button size="sm" onClick={ () =>  setOpen(false)} className="corsor-pointer">
+              <Button
+                size="sm"
+                onClick={() => setOpen(false)}
+                className="corsor-pointer"
+              >
                 <CircleX className="text-black" />
               </Button>
             </div>
@@ -37,7 +64,7 @@ const Chatbord = () => {
             </div>
             <div className="p-2 overflow-auto h-[465px] space-y-4">
               <div className="flex">
-                <p className="text-[12px] bg-white text-black rounded-2xl rounded-tl-none py-2 px-3 text-justify w-67 break-words">
+                <p className="text-[12px] bg-[#ffffff88] shadow-md text-black rounded-2xl rounded-tl-none py-2 px-3 text-justify w-67 break-words">
                   Hello! I am Rehan's AI assistant. You can ask me anything
                   about Rehan's portfolio, projects, or skills. How can I help
                   you ?
@@ -49,10 +76,24 @@ const Chatbord = () => {
                   hi Rehan how are you?
                 </p>
               </div>
+              {typing && (
+                <div className="flex justify-end">
+                  <p className="text-[12px] text-white bg-black px-3 py-1 rounded-2xl rounded-tr-none max-w-xs break-words">
+                    <Bouncy size="30" speed="1.75" color="white" />
+                  </p>
+                </div>
+              )}
             </div>
-            <form  className="flex w-full items-center justify-center gap-2.5 border-black border-t p-2.5">
-               <Input placeholder="Ask a question"></Input>
-               <Button className=" cursor-pointer bg-[linear-gradient(#ffffff33),linear-gradient(#fff_50%,rgba(255,255,255,0.6)_80%,rgba(0,0,0,0)),linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))] bg-[length:200%] text-foreground [background-clip:padding-box,border-box,border-box] [background-origin:border-box] [border:calc(0.08*1rem)_solid_transparent] before:absolute before:bottom-[-20%] before:left-1/2 before:z-[0] before:h-[20%] before:w-[60%] before:-translate-x-1/2 before:animate-rainbow before:bg-[linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))] before:[filter:blur(calc(0.8*1rem))] dark:bg-[linear-gradient(#121213,#121213),linear-gradient(#121213_50%,rgba(18,18,19,0.6)_80%,rgba(18,18,19,0)),linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))]"><Send className="h-4 w-4" /></Button>
+            <form className="flex w-full items-center justify-center gap-2.5 border-black border-t p-2.5">
+              <Input
+                onChange={handleTyping}
+                className="bg-[linear-gradient(#ffffff33)"
+                ref={inputref}
+                placeholder="Ask a question"
+              ></Input>
+              <Button className=" cursor-pointer bg-[linear-gradient(#ffffff33),linear-gradient(#fff_50%,rgba(255,255,255,0.6)_80%,rgba(0,0,0,0)),linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))] bg-[length:200%] text-foreground [background-clip:padding-box,border-box,border-box] [background-origin:border-box] [border:calc(0.08*1rem)_solid_transparent] before:absolute before:bottom-[-20%] before:left-1/2 before:z-[0] before:h-[20%] before:w-[60%] before:-translate-x-1/2 before:animate-rainbow before:bg-[linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))] before:[filter:blur(calc(0.8*1rem))] dark:bg-[linear-gradient(#121213,#121213),linear-gradient(#121213_50%,rgba(18,18,19,0.6)_80%,rgba(18,18,19,0)),linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))]">
+                <Send className="h-4 w-4" />
+              </Button>
             </form>
           </div>
         )}
